9f0bce45f2be90fa1e6268f7a703d9323aa2b314
alexiemaglalang13-design <alexiemaglalang13@gmail.com>
2025-12-04 13:13:34 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Candy Store</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <h1>The Biscoff Store</h1>
+    <nav>
+      <a href="index.php">Home</a> | 
+      <a href="biscoff.php">Product</a> | 
+      <a href="about.php">About</a> | 
+      <a href="contact.php">Contact</a>
+    </nav>
